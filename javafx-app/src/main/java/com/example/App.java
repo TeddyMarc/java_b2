5b4fedb5beed9990fc33bec8c78ffc364c8cdf77
TeddyMarc <teddymarc80@gmail.com>
2025-03-10 12:31:59 +0100

    Message

--- javafx-app/src/main/java/com/example/App.java
+++ javafx-app/src/main/java/com/example/App.java
@@ -1,19 +1,24 @@
 package com.example;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+//import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception{
         // Créer un label
-        Label label = new Label("Bonjour, JavaFX !");
+        //Label label = new Label("Bonjour, JavaFX !");//
+ 
+         // Charger l'interface FXML
+        VBox root = FXMLLoader.load(getClass().getResource("/layouts/App.fxml"));
 
         // Créer une scène avec le label
-        Scene scene = new Scene(label, 300, 200);
+        Scene scene = new Scene(root, 300, 200);
         scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
 
         // Configurer la fenêtre principale
